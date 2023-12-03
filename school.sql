3e25b3c6c060780e9a0a79cdc4e4d9ad0652e571
Asare Thomas Appiah <31077279+Asare27@users.noreply.github.com>
2023-12-03 13:10:55 +0000

    Last Changes

--- school.sql
+++ school.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2023 at 06:56 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Dec 03, 2023 at 02:03 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `school`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL,
+  `indexNumber` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `otherName` varchar(255) NOT NULL,
+  `dateOfAdmission` date NOT NULL,
+  `nameOfFormerSchool` varchar(255) NOT NULL,
+  `dateOfBirth` date NOT NULL,
+  `gender` enum('male','female') DEFAULT NULL,
+  `religiousDenomination` varchar(255) NOT NULL,
+  `phoneNumber` varchar(20) NOT NULL,
+  `hometown` varchar(255) NOT NULL,
+  `placeOfBirth` varchar(255) NOT NULL,
+  `district` varchar(255) NOT NULL,
+  `region` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -44,11 +68,18 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `registration_date`) VAL
 (3, 'John Doe', 'tho@yahoo.com', '$2y$10$Cm2ugtGdXtIZjHSBQm6WXOeLx.wbgZdUkIhUWct5xzPd7h3zWrD5a', '2023-11-26 17:59:27'),
 (4, 'Josephine Tsorhey', 'jtsorhey@gmail.com', '$2y$10$4gV4oN1HFza.BQmGezzrUO7ZvVePwFVICfh3vn51WFCSZBKkpyG5a', '2023-11-26 18:38:26'),
 (6, 'Josephine Tsorhey', 'jsorhey@gmail.com', '$2y$10$3dRyXu6tI1V.wVsebec0leuKzQ7f87s4Ok8xlMnUXsWy2Xhdq6ZFe', '2023-11-26 18:40:19'),
-(7, 'Frank Attitsogbui', 'frank@yahoo.com', '$2y$10$2w6LehSalMtulP3mQh5lvuD2iYXp0RVmkB0h4Sza9XkU8eeyqaAUO', '2023-11-26 22:18:06');
+(7, 'Frank Attitsogbui', 'frank@yahoo.com', '$2y$10$2w6LehSalMtulP3mQh5lvuD2iYXp0RVmkB0h4Sza9XkU8eeyqaAUO', '2023-11-26 22:18:06'),
+(8, 'Simon', 'simon@yahoo.com', '$2y$10$HH2N1qiNPFDcZ2Lac0Q9reR24u6wf1UuB56vpONjpjgB.sNt8dEha', '2023-11-27 11:19:00');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -62,10 +93,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
