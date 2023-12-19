20411e312d05d4940ec5e5a013f5d0bee8354cf1
Asare Thomas Appiah <31077279+Asare27@users.noreply.github.com>
2023-12-19 15:55:09 +0000

    There are some changes in the display of the table, the view has been reduced

--- display.php
+++ display.php
@@ -5,8 +5,8 @@
         $table = '<table class="table">
         <thead class="table-dark">
             <tr>
-                <th scope="col"> SI No</th>
-                <th scope="col"> IndexNo </th>
+                <th scope="col">SI No</th>
+                <th scope="col">IndexNo </th>
                 <th scope="col">FirstName </th>
                 <th scope="col">LastName </th>
                 <th scope="col">OtherName </th>
@@ -14,11 +14,7 @@
                 <th scope="col">Former School </th>
                 <th scope="col">DOB </th>
                 <th scope="col">Gender </th>
-                <th scope="col">Denomination </th>
                 <th scope="col">Contact </th>
-                <th scope="col">Home Town </th>
-                <th scope="col">Birth Place </th>
-                <th scope="col">District </th>
                 <th scope="col">Region </th>
                 <th scope="col">Actions </th>
             </tr>
@@ -29,6 +25,7 @@
 
         while($row = mysqli_fetch_assoc($result)) {
             $id = $row['id'];
+            $indexNumber = $row['indexNumber'];
             $firstName = $row['firstName'];
             $lastName = $row['lastName'];
             $otherName = $row['otherName'];
@@ -36,26 +33,19 @@
             $nameOfFormerSchool = $row['nameOfFormerSchool'];
             $dateOfBirth = $row['dateOfBirth'];
             $gender = $row['gender'];
-            $religiousDenomination = $row['religiousDenomination'];
             $phoneNumber = $row['phoneNumber'];
-            $hometown = $row['hometown'];
-            $placeOfBirth = $row['placeOfBirth'];
-            $district = $row['district'];
             $region = $row['region'];
             $table .= '<tr>
                 <td scope="row">'.$number.'</td>
+                <td>'. $indexNumber   .'</td>
                 <td>'. $firstName .' </td>
                 <td>'. $lastName .' </td>
                 <td>'. $otherName.' </td>
                 <td>'. $dateOfAdmission  .' </td>
                 <td>'. $nameOfFormerSchool .' </td>
                 <td>'. $dateOfBirth .' </td>
-                <td>'. $gender .' </td>
-                <td>'. $religiousDenomination .' </td>
+                <td>'. $gender .' </td> 
                 <td>'. $phoneNumber .' </td>
-                <td>'. $hometown .' </td>
-                <td>'. $placeOfBirth  .' </td>
-                <td>'. $district  .' </td>
                 <td>'. $region  .' </td>
                 <td>
                     <button class="btn btn-dark">Update</button>
